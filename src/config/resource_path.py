e629c415b6481b2d8d89c14fc78c4f88c6b54175
Tonatiuh <programador.jr@coorsamexico.com>
2025-06-05 10:05:05 -0600

    correction pusher .exe

--- src/config/resource_path.py
+++ src/config/resource_path.py
@@ -1,10 +1,15 @@
 import sys
 import os
 
+
+
 def resource_path(*relative_paths):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, *relative_paths)
     return os.path.join(os.getcwd(), *relative_paths)
+
+
+os.environ['SSL_CERT_FILE'] = resource_path('assets/certifi/cacert.pem')
 
 # Ejemplo de uso
 #ruta_logo = resource_path('assets/logo.png')
